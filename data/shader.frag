a051d5614108e08639c34b47f80ba2c87c52c355
Kaporos <theo@daron.be>
2023-11-09 11:05:35 +0100

    adding dragging + infinite zoom + beautiful colors + burning ship

--- data/shader.frag
+++ data/shader.frag
@@ -10,6 +10,7 @@ uniform float zoom_factor;
 uniform int fractal_type;
 uniform float precision;
 uniform float threshold;
+uniform vec2 offset;
 vec2 squareComplex(vec2 complex) {
     return vec2(complex.x * complex.x - complex.y*complex.y, 2.0*complex.x*complex.y);
 }
@@ -19,18 +20,42 @@ float absComplex(vec2 complex) {
 }
 
 void color(int iters, float val) {
-    gl_FragColor = vec4(iters/precision, 0.0,0.0, 1.0);
+    vec4 gradients[17];
+    gradients[0] = vec4(66.0, 30.0, 15.0, 255.0);
+    gradients[1] = vec4(25.0, 7.0, 26.0, 255.0);
+    gradients[3] = vec4(9.0, 1.0, 47.0, 255.0);
+    gradients[4] = vec4(4.0, 4.0, 73.0, 255.0);
+    gradients[5] = vec4(0.0, 7.0, 100.0, 255.0);
+    gradients[6] = vec4(12.0, 44.0, 138.0, 255.0);
+    gradients[7] = vec4(24.0, 82.0, 177.0, 255.0);
+    gradients[8] = vec4(57.0, 125.0, 209.0, 255.0);
+    gradients[9] = vec4(134.0, 181.0, 229.0, 255.0);
+    gradients[10] = vec4(211.0, 236.0, 248.0, 255.0);
+    gradients[11] = vec4(241.0, 233.0, 191.0, 255.0);
+    gradients[12] = vec4(248.0, 201.0, 95.0, 255.0);
+    gradients[13] = vec4(255.0, 170.0, 0.0, 255.0);
+    gradients[14] = vec4(204.0, 128.0, 0.0, 255.0);
+    gradients[15] = vec4(153.0, 87.0, 0.0, 255.0);
+    gradients[16] = vec4(106.0, 52.0, 3.0, 255.0);
+
+
+    
+    gl_FragColor = gradients[iters % 16] / 255.0;
 }
 void compute_fractal(vec2 c) {
     vec2 f = u_constant; //u_constant defined in cpp.
     vec2 z;
-    if (fractal_type == 0) {
-        z = f; //Mandelbrot case
+    if (fractal_type != 1) {
+        z = f; //Mandelbrot case and Burning ship
     } else {
         z = c; //Julia case
         c = f;
     }
     for (int x = 0; x < precision; x ++) {
+        if (fractal_type == 2) { //Burning ship
+            z.x = abs(z.x);
+            z.y = abs(z.y);
+        }
         z = squareComplex(z) + c;
         float a = absComplex(z);
         if (a > threshold) {
@@ -44,9 +69,7 @@ void compute_fractal(vec2 c) {
 
 
 void main() {
-    vec2 st = gl_FragCoord.st/u_resolution;
-    vec2 c = vec2(st.x, 1-st.y);
-    c = (c * 2)-1; // going from 0;1 to -1:1
-    c = c / zoom_factor;
-    compute_fractal(c);
+    vec2 st = (gl_FragCoord.st - u_resolution*0.5) / (zoom_factor);
+    st = (st + offset)/u_resolution;
+    compute_fractal(st);
 } 
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/Graphics/CircleShape.hpp>
 #include <SFML/Graphics/Color.hpp>
 #include <SFML/Graphics/Glsl.hpp>
 #include <SFML/Graphics/RectangleShape.hpp>
@@ -6,62 +7,115 @@
 #include <SFML/Graphics/Shader.hpp>
 #include <SFML/System/Clock.hpp>
 #include <SFML/System/Vector2.hpp>
+#include <cstdio>
 #include <imgui.h>
 #include <imgui-SFML.h>
 
+//CONSTANTS
 const int WINDOW_HEIGHT = 1000;
 const int WINDOW_WIDTH = 1000;
-static float zoom_factor = 0.5;
-static int fractal_type = 1;
+const int FRACTAL_HEIGHT = WINDOW_HEIGHT;
+const int FRACTAL_WIDTH = WINDOW_WIDTH;
+//SHADER PARAMETERS
+static double zoom_factor = 0.3;
+static int fractal_type = 1; // 0 - Mandelbrot; 1 - Julia Set ; 2 - Burning ship
 static ImVec2 mandel_constant(0.0f,0.0f);
 static ImVec2 julia_constant(0.258f,0.001f);                
-static float precision = 250;
+static float precision = 750;
 static float threshold = 50;
+static float offset[2] = {0.0f, 0.0f};
+
+//PARAMETERS
+static double zoom_speed = 0.002;
+
+//STATE
+static bool dragging = false;
+static float dragStart[2] = {0.0f, 0.0f};
+static sf::Vector2f mouse_position = sf::Vector2f{};
+static bool infinite_zoom = false;
+static bool show_pointer = true;
 
 
 int main() {
 
-  sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH,WINDOW_HEIGHT), "Fractals");
+  //Setup of window
+  sf::RenderWindow window(sf::VideoMode(FRACTAL_WIDTH,FRACTAL_HEIGHT), "Fractals");
   if (!ImGui::SFML::Init(window)) {
     return 1;
   };
-  
-  
-  sf::Clock deltaClock;
-  sf::Clock clock;
 
-  sf::RectangleShape shape(sf::Vector2f(window.getSize()));
+  
+  //Setup of time tracking. 
+  sf::Clock deltaClock;
+
+  //Simple shape the shader will draw on.
+  sf::RectangleShape shaderOutput(sf::Vector2f(window.getSize()));
+  sf::CircleShape circle(5);
+  circle.setFillColor(sf::Color::Red);
 
   sf::Shader shader;
   if (!shader.loadFromFile("data/shader.frag", sf::Shader::Fragment)) {
     return -1;
   }
 
-  sf::Vector2f mouse_position = sf::Vector2f{};
   while (window.isOpen())
   {
       //Event handling
       sf::Event event;
       while (window.pollEvent(event)) {
         ImGui::SFML::ProcessEvent(window, event);
+        ImGuiIO& im = ImGui::GetIO();
+        
           if (event.type == sf::Event::Closed) {
             window.close();
           }
           if (event.type == sf::Event::MouseMoved) {
             mouse_position = window.mapPixelToCoords({event.mouseMove.x, event.mouseMove.y});
           }
+
+          //If i am interacting with ImGUI i dont want to care about mouse events.
+          if (im.WantCaptureMouse) {
+            break;
+          }
+
+          //Zoom + Drag handling
+          if (event.type == sf::Event::MouseButtonPressed) {
+            //Drag start
+            dragging = true;
+            auto pos = window.mapCoordsToPixel(mouse_position);
+            dragStart[0] = pos.x;
+            dragStart[1] = pos.y;
+          } 
+          if (event.type == sf::Event::MouseButtonReleased) {
+            dragging = false;
+          }
           if (event.type == sf::Event::MouseWheelScrolled) {
-            zoom_factor += event.mouseWheelScroll.delta * 0.2;
-            zoom_factor = std::max(zoom_factor, 0.1f);
-          }    
+            //Zoom
+            zoom_factor += event.mouseWheelScroll.delta * 0.2 * zoom_factor;
+            zoom_factor = std::max(zoom_factor, 0.1);
+          }
+          if (event.type == sf::Event::MouseMoved && dragging) {
+            //Drag offset computation
+            float posX = event.mouseMove.x;
+            float posY = event.mouseMove.y;
+            offset[0] += -1 * (posX - dragStart[0]) * (1/zoom_factor);
+            offset[1] -= -1 * (posY - dragStart[1]) * (1/zoom_factor);
+            auto pos = window.mapCoordsToPixel(mouse_position);
+            dragStart[0] = pos.x;
+            dragStart[1] = pos.y;
+          }
       }
+      if (infinite_zoom) {
+        zoom_factor += zoom_speed * zoom_factor;
+      }
+
+      //Giving params to my shader.
       shader.setUniform("u_resolution", sf::Glsl::Vec2{ window.getSize() });
-      shader.setUniform("u_mouse", sf::Glsl::Vec2{ mouse_position });
-      shader.setUniform("u_time", clock.getElapsedTime().asSeconds());
-      shader.setUniform("zoom_factor", zoom_factor);
+      shader.setUniform("zoom_factor", (float) zoom_factor);
       shader.setUniform("fractal_type", fractal_type);
       shader.setUniform("precision", precision);
       shader.setUniform("threshold", threshold);
+      shader.setUniform("offset", sf::Glsl::Vec2{offset[0], offset[1]});
 
 
       ImVec2* constant = &mandel_constant;
@@ -71,16 +125,24 @@ int main() {
       shader.setUniform("u_constant", sf::Glsl::Vec2{(*constant).x, (*constant).y});
 
       ImGui::SFML::Update(window, deltaClock.restart());
-      const char* fractal_types[2] = {"Mandelbrot", "Julia"};
+      const char* fractal_types[3] = {"Mandelbrot", "Julia", "Burning ship"};
       ImGui::Begin("Fractals config");
-      ImGui::Combo("Fractal type", &fractal_type, fractal_types, 2);
-      ImGui::SliderFloat("Precision", &precision, 25.0, 1000.0);
+      ImGui::Combo("Fractal type", &fractal_type, fractal_types, IM_ARRAYSIZE(fractal_types));
+      ImGui::SliderFloat("Precision", &precision, 25.0, 10000.0);
       ImGui::SliderFloat("Threshold", &threshold, 25.0, 1000.0);
-      if (fractal_type == 0) {
+      if (fractal_type == 0 || fractal_type == 0) {
         ImGui::DragFloat2("Constant", (float*) &mandel_constant, 0.005f, -2.0, 2.0);
       }
       if (fractal_type == 1) {
         ImGui::DragFloat2("Constant", (float*) &julia_constant, 0.005f, -2.0, 2.0);
+      }
+      ImGui::Checkbox("Show zoom pointer", &show_pointer);
+      ImGui::InputDouble("Zoom", &zoom_factor);
+
+      ImGui::InputDouble("Infinite zoom speed", &zoom_speed);
+      ImGui::Checkbox("Infinite zoom", &infinite_zoom);
+      if (ImGui::Button("Reset zoom")) {
+        zoom_factor = 0.3;
       }
       ImGui::End();
     
@@ -88,7 +150,12 @@ int main() {
 
       //rendering part
       window.clear(sf::Color::Black);
-      window.draw(shape, &shader);
+      window.draw(shaderOutput, &shader);
+      if (show_pointer) {
+      
+      circle.setPosition(window.getSize().x/2.0 - circle.getRadius(), window.getSize().y/2.0 - circle.getRadius());
+      window.draw(circle);
+      }
       ImGui::SFML::Render(window);
       window.display();
       
